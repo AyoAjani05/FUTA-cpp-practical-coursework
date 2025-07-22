cd428ad6bc179ff37a3f01fa87ff28f92cf9477b
Ayo Ajani <114166504+AyoAjani05@users.noreply.github.com>
2025-07-22 08:44:20 +0100

    Add files via upload

---
+++ 2. variable.cpp
@@ -0,0 +1,15 @@
+// VARIABLE EXAMPLE
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //[DATA TYPE] VARIABLE NAME;
+    int x; // VARIABLE DECLARATION
+    x = 6; 
+    int y;
+    y = x + 5; //6 + 5 = 11
+    cout << "The Value of y is: "<<y;
+    return 0;
+}
